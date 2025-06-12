eea30377df11393ed90f13977ae428e99890c21c
lola <2213948@chester.ac.uk>
2025-06-12 17:49:03 +0100

    start again

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'signIn.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,46 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Image1(),
-        ),
-      ),
-    );
-  }
-}
-
-class Image1 extends StatelessWidget {
-  const Image1({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Container(
-          width: 270,
-          height: 270,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/mainLogo.png"),
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-        Container(
-          width: 394,
-          height: 311,
-          clipBehavior: Clip.antiAlias,
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment(0.50, 0.0),
-              end: Alignment(0.50, 1.00),
-              colors: [Colors.white, Color(0xFF5C865F)],
-            ),
-          ),
-        ),
-      ],
+      title: 'My App',
+      debugShowCheckedModeBanner: false,
+      home: SignInPage(), 
     );
   }
 }
