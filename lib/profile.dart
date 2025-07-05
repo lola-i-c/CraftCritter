05c2795eb0c1b7e28fdb07e5a9aa0f50b99fa592
lola <2213948@chester.ac.uk>
2025-07-05 17:25:54 +0100

    added a gif to profile page

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,57 +1,56 @@
 import 'package:craft_critter_application/home.dart';
+import 'package:craft_critter_application/sign_in.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          const Text('Profile'),
 
-          //background gradient***********************************************
-          Container(
-            width: 394,
-            height: 311,
-            clipBehavior: Clip.antiAlias,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment(0.50, -0.00),
-                end: Alignment(0.50, 1.00),
-                colors: [Colors.white, const Color(0xFF5C865F)],
-              ),
-            ),
-          ),
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    body: Column(
+      children: [
+        const Text('Profile'),
 
-          //logo***********************************************
-          Container(
+ Container(
             width: 270,
             height: 270,
             decoration: const BoxDecoration(
               image: DecorationImage(
-                image: AssetImage("assets/mainLogo.png"),
+                image: AssetImage("assets/leaf.gif"),
                 fit: BoxFit.cover,
               ),
             ),
           ),
 
-          TextButton(
-            style: ButtonStyle(
-              foregroundColor: WidgetStateProperty.all<Color>(Colors.blue),
-            ),
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(builder: (context) => const HomePage()),
-              );
-            }, 
-            child: Text('Home Page'),
-            
+
+        TextButton(
+          style: ButtonStyle(
+            foregroundColor: WidgetStateProperty.all<Color>(Colors.blue),
           ),
-        ],
-      ),
-    );
-  }
+          onPressed: () {
+            Navigator.of(
+              context,
+            ).push(MaterialPageRoute(builder: (context) => const HomePage()));
+          },
+          child: Text('Home Page'),
+        ),
+
+        TextButton(
+          style: ButtonStyle(
+            foregroundColor: WidgetStateProperty.all<Color>(Colors.blue),
+          ),
+          onPressed: () {
+            Navigator.of(
+              context,
+            ).push(MaterialPageRoute(builder: (context) => const SignInPage()));
+          },
+          child: Text('Sign Out'),
+        ),
+      ],
+    ),
+  );
+}
 }
